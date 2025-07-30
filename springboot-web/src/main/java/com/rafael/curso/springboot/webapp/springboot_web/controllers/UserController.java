2da4a19c6ec1f45a530ef285cee581571abffdbb
Marvin Meza <101306102+Meza2805@users.noreply.github.com>
2025-07-30 16:49:06 -0600

    last changes fot today

--- springboot-web/src/main/java/com/rafael/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ springboot-web/src/main/java/com/rafael/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -5,8 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.rafael.curso.springboot.webapp.springboot_web.entities.User;
 import com.rafael.curso.springboot.webapp.springboot_web.repositories.UserRepository;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -24,6 +27,13 @@ public class UserController {
 @PostMapping("/create")
 public ResponseEntity<String> createUser(@RequestBody User user) ///Sirve para hacer una insercion de usuario uno a uno
 {
+    ///validar que el correo tenga un formato correcto 
+    if (user.getEmail() != null && !user.getEmail().matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body("❌ Error: El formato del correo electrónico es incorrecto.");
+    }       
+
     if (user.getName() == null || user.getEmail() == null) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
@@ -35,4 +45,16 @@ public ResponseEntity<String> createUser(@RequestBody User user) ///Sirve para h
             .body("✅ Usuario insertado correctamente en la base de datos.");
     }
 
+
+
+  @GetMapping("/list_users")
+public ResponseEntity<List<User>> getAllUsers() 
+{
+    List<User> users = userRepository.findAll();
+    if (users.isEmpty()) {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+    return ResponseEntity.ok(users);
+}
+  
 }
